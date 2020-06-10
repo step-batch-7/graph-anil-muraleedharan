c68d1bb3a1391bd43cbd7140c5a069552048dc5c
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-06-10 20:42:52 +0530

    Added functions generateAdjacencyTable and bfs.

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,26 @@
-//Example 
-// Pairs => [[from,to],[to,from]]
-// Source => from
-// To => to 
-// Should return true.
-
-const bfs = function(pairs,source,target){
-  
+const generateAdjacencyTable = (links) => {
+  const table = {};
+  links.forEach(([src, dest]) => {
+    if (!table[src]) table[src] = [];
+    table[src].push(dest);
+  });
+  return table;
 };
 
-module.exports = {bfs};
+const bfs = function (pairs, source, target) {
+  adjacencyTable = generateAdjacencyTable(pairs);
+  const queue = [source];
+  const visited = [];
+  while (queue.length != 0) {
+    const currentNode = queue.shift();
+    if (adjacencyTable[currentNode].includes(target)) return true;
+    adjacencyTable[currentNode].forEach((child) => {
+      isAlreadyExist = queue.includes(child) || visited.includes(child);
+      if (!isAlreadyExist) queue.push(child);
+    });
+    visited.push(currentNode);
+  }
+  return false;
+};
+
+module.exports = { bfs };
