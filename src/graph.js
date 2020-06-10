11e341d6e47898698a966a72f95998ddd935ed8f
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-06-10 23:22:13 +0530

    Added a condition is the entry exisits in table.

--- src/graph.js
+++ src/graph.js
@@ -13,12 +13,14 @@ const bfs = function (pairs, source, target) {
   const visited = [];
   while (queue.length != 0) {
     const currentNode = queue.shift();
-    if (adjacencyTable[currentNode].includes(target)) return true;
-    adjacencyTable[currentNode].forEach((child) => {
-      isAlreadyExist = queue.includes(child) || visited.includes(child);
-      if (!isAlreadyExist) queue.push(child);
-    });
-    visited.push(currentNode);
+    if (adjacencyTable[currentNode]) {
+      if (adjacencyTable[currentNode].includes(target)) return true;
+      adjacencyTable[currentNode].forEach((child) => {
+        isAlreadyExist = queue.includes(child) || visited.includes(child);
+        if (!isAlreadyExist) queue.push(child);
+      });
+      visited.push(currentNode);
+    }
   }
   return false;
 };
